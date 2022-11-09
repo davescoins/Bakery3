dcf001bc566a67545e3084f5c499de0bf04a504f
SamsonJy <Samsonjy@gwu.edu>
2022-11-08 23:06:13 -0500

    new

---
+++ app/src/main/java/com/example/bakery/Activity_demographic.java
@@ -0,0 +1,14 @@
+package com.example.bakery;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_demographic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demographic);
+    }
+}
