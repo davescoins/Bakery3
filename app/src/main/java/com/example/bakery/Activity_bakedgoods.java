edc6838bcde5b763421f9344bb7344693508f9dd
SamsonJy <Samsonjy@gwu.edu>
2022-11-10 11:21:20 -0500

    error fix but keep stopping

--- app/src/main/java/com/example/bakery/Activity_bakedgoods.java
+++ app/src/main/java/com/example/bakery/Activity_bakedgoods.java
@@ -9,6 +9,6 @@ public class Activity_bakedgoods extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bakedgoods);
+        setContentView(R.layout.atctivity_bakegoods);
     }
 }
--- app/src/main/java/com/example/bakery/Activity_main.java
+++ app/src/main/java/com/example/bakery/Activity_main.java
@@ -1,14 +1,26 @@
 package com.example.bakery;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_main extends AppCompatActivity {
+    private Button mBtnStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnStartButton = findViewById(R.id.main_startButton);
+        mBtnStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Activity_main.this, Activity_bakedgoods.class);
+                startActivity(intent);
+            }
+        });
     }
 }
